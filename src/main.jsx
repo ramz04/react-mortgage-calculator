297e3059c69d5c84823fc645767c58889a04c33d
ramz04 <wunpini95@gmail.com>
2023-11-06 08:43:28 +0000

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import { ThemeProvider } from "@mui/material/styles"
+import { CssBaseline } from "@mui/material/CssBaseline"
+import { theme } from "./Theme.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <App />
+      <CssBaseline />
+    </ThemeProvider>
+  </React.StrictMode>
 )
